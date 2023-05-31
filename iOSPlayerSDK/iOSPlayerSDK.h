78138f9203b70364b6167bae00f2b312a5092926
Bharath <bharath@testpress.in>
2023-05-31 12:11:57 +0530

    Initial commit

---
+++ iOSPlayerSDK/iOSPlayerSDK.h
@@ -0,0 +1,18 @@
+//
+//  iOSPlayerSDK.h
+//  iOSPlayerSDK
+//
+//  Created by Bharath on 30/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSPlayerSDK.
+FOUNDATION_EXPORT double iOSPlayerSDKVersionNumber;
+
+//! Project version string for iOSPlayerSDK.
+FOUNDATION_EXPORT const unsigned char iOSPlayerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSPlayerSDK/PublicHeader.h>
+
+
