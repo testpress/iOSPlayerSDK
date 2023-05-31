1c0691526fce97ba1a92822bbd0b99270ea720ed
Bharath <bharath@testpress.in>
2023-05-31 12:24:10 +0530

    Update gitignore and add base player view

---
+++ iOSPlayerSDK/Views/TPStreamPlayer.swift
@@ -0,0 +1,20 @@
+//
+//  TPStreamPlayer.swift
+//  iOSPlayerSDK
+//
+//  Created by Bharath on 31/05/23.
+//
+
+import SwiftUI
+
+struct TPStreamPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TPStreamPlayer_Previews: PreviewProvider {
+    static var previews: some View {
+        TPStreamPlayer()
+    }
+}
