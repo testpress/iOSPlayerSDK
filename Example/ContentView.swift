b72b21cffe6e0fae1e15323b29a7ce7ee1982382
Bharath <bharath@testpress.in>
2023-06-01 18:00:19 +0530

    Rename SDK as TPStreamsSDK

--- Example/ContentView.swift
+++ Example/ContentView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import iOSPlayerSDK
+import TPStreamsSDK
 
 struct ContentView: View {
     var body: some View {
--- Example/ExampleApp.swift
+++ Example/ExampleApp.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import iOSPlayerSDK
+import TPStreamsSDK
 
 import UIKit
 
