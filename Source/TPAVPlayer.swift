8d8309d93d2768ce1013d9927417937d989ff59f
hari-testpress <hari@testpress.in>
2023-06-02 15:18:59 +0530

    refactor: Refactor TPAVPlayer

--- Source/TPAVPlayer.swift
+++ Source/TPAVPlayer.swift
@@ -10,43 +10,40 @@ import AVKit
 
 
 public class TPAVPlayer: AVPlayer {
-    var accessToken: String?
-    var assetID: String?
+    private var accessToken: String
+    private var assetID: String
     
     public init(assetID: String, accessToken: String) {
         guard TPStreamsSDK.orgCode != nil else {
             fatalError("You must call TPStreamsSDK.initialize")
         }
         
-        if (accessToken.isEmpty || assetID.isEmpty) {
+        if accessToken.isEmpty || assetID.isEmpty {
             fatalError("AccessToken/AssetID cannot be empty")
         }
         self.accessToken = accessToken
         self.assetID = assetID
 
         super.init()
-        API.getAsset(assetID, accessToken) {[weak self] asset, error in
+        
+        loadAsset()
+    }
+    
+    private func loadAsset() {
+        API.getAsset(assetID, accessToken) { [weak self] asset, error in
             guard let self = self else { return }
-            self.APIResponse(asset, error)
+            self.handleAPIResponse(asset: asset, error: error)
         }
     }
     
-    override init() {
-        super.init()
-    }
-    
-    override init(url URL: URL) {
-        super.init(url: URL)
-    }
-
-    override init(playerItem item: AVPlayerItem?) {
-        super.init(playerItem: item)
-    }
-    
-    func APIResponse(_ asset: API.Asset?, _ error: Error?) {
+    private func handleAPIResponse(asset: API.Asset?, error: Error?) {
         if let asset = asset {
-            let url = URL(string: asset.video.playbackURL)
-            let avURLAsset = AVURLAsset(url: url!)
+            guard let url = URL(string: asset.video.playbackURL) else {
+                debugPrint("Got invalid playback URL from API")
+                return
+            }
+            
+            let avURLAsset = AVURLAsset(url: url)
             self.prepareAssetForDRMPlayback(avURLAsset)
             let playerItem = AVPlayerItem(asset: avURLAsset)
             self.replaceCurrentItem(with: playerItem)
@@ -55,8 +52,8 @@ public class TPAVPlayer: AVPlayer {
         }
     }
     
-    func prepareAssetForDRMPlayback(_ avURLAsset: AVURLAsset) {
+    private func prepareAssetForDRMPlayback(_ avURLAsset: AVURLAsset) {
         ContentKeyManager.shared.contentKeySession.addContentKeyRecipient(avURLAsset)
-        ContentKeyManager.shared.contentKeyDelegate.setAssetDetails(assetID!, accessToken!)
+        ContentKeyManager.shared.contentKeyDelegate.setAssetDetails(assetID, accessToken)
     }
 }
