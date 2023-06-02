306f30c9f2200bf829400590752f57434565ac99
Hari <hari@testpress.in>
2023-06-02 18:47:02 +0530

    refactor: Refactor TPAVPlayer (#1)
    - Made `accessToken`, `assetID`, and methods marked as private to encapsulate their access within the class.
    - Removed unnecessary initializers, keeping only the one that allows initialization with asset ID and access token. Other initializers are incompatible with TPAVPlayer's expected behavior

--- Source/TPAVPlayer.swift
+++ Source/TPAVPlayer.swift
@@ -10,53 +10,54 @@ import AVKit
 
 
 public class TPAVPlayer: AVPlayer {
-    var accessToken: String?
-    var assetID: String?
+    private var accessToken: String
+    private var assetID: String
     
     public init(assetID: String, accessToken: String) {
         guard TPStreamsSDK.orgCode != nil else {
             fatalError("You must call TPStreamsSDK.initialize")
         }
         
-        if (accessToken.isEmpty || assetID.isEmpty) {
+        if accessToken.isEmpty || assetID.isEmpty {
             fatalError("AccessToken/AssetID cannot be empty")
         }
         self.accessToken = accessToken
         self.assetID = assetID
 
         super.init()
-        API.getAsset(assetID, accessToken) {[weak self] asset, error in
+        fetchAsset()
+    }
+    
+    private func fetchAsset() {
+        API.getAsset(assetID, accessToken) { [weak self] asset, error in
             guard let self = self else { return }
-            self.APIResponse(asset, error)
+            
+            if let asset = asset {
+                self.setup(withAsset: asset)
+            } else if let error = error{
+                debugPrint(error.localizedDescription)
+            }
         }
     }
     
-    override init() {
-        super.init()
-    }
-    
-    override init(url URL: URL) {
-        super.init(url: URL)
-    }
-
-    override init(playerItem item: AVPlayerItem?) {
-        super.init(playerItem: item)
-    }
-    
-    func APIResponse(_ asset: API.Asset?, _ error: Error?) {
-        if let asset = asset {
-            let url = URL(string: asset.video.playbackURL)
-            let avURLAsset = AVURLAsset(url: url!)
-            self.prepareAssetForDRMPlayback(avURLAsset)
-            let playerItem = AVPlayerItem(asset: avURLAsset)
-            self.replaceCurrentItem(with: playerItem)
-        } else if let error = error {
-            debugPrint(error.localizedDescription)
+    private func setup(withAsset asset: API.Asset) {
+        guard let url = URL(string: asset.video.playbackURL) else {
+            debugPrint("Invalid playback URL received from API: \(asset.video.playbackURL)")
+            return
         }
+        
+        let avURLAsset = AVURLAsset(url: url)
+        self.setPlaybackURL(avURLAsset)
+        self.setupDRM(avURLAsset)
     }
     
-    func prepareAssetForDRMPlayback(_ avURLAsset: AVURLAsset) {
+    private func setPlaybackURL(_ asset: AVURLAsset){
+        let playerItem = AVPlayerItem(asset: asset)
+        self.replaceCurrentItem(with: playerItem)
+    }
+    
+    private func setupDRM(_ avURLAsset: AVURLAsset) {
         ContentKeyManager.shared.contentKeySession.addContentKeyRecipient(avURLAsset)
-        ContentKeyManager.shared.contentKeyDelegate.setAssetDetails(assetID!, accessToken!)
+        ContentKeyManager.shared.contentKeyDelegate.setAssetDetails(assetID, accessToken)
     }
 }
