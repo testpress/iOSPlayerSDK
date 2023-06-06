8a7ae641d3cc3ba60c08025870e5fd31206f6295
Hari <hari@testpress.in>
2023-06-06 16:56:26 +0530

    feat: Add controls to rewind/forward 10 seconds (#4)

---
+++ Source/Views/MediaControlsView.swift
@@ -0,0 +1,49 @@
+//
+//  MediaControlsView.swift
+//  TPStreamsSDK
+//
+//  Created by Testpress on 06/06/23.
+//
+
+import SwiftUI
+
+struct MediaControlsView: View {
+    @EnvironmentObject var player: TPStreamPlayer
+    
+    var body: some View {
+        HStack() {
+            Spacer()
+            Button(action: player.rewind) {
+                Image("rewind", bundle: bundle)
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .brightness(-0.1)
+            }
+            Spacer()
+            Button(action: togglePlay) {
+                Image(player.status == .paused ? "play" : "pause", bundle: bundle)
+                    .resizable()
+                    .frame(width: 48, height: 48)
+                    .brightness(-0.1)
+            }
+            Spacer()
+            Button(action: player.forward) {
+                Image("forward", bundle: bundle)
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .brightness(-0.1)
+            }
+            Spacer()
+        }
+    }
+    
+    
+    public func togglePlay(){
+        if player.status == .paused {
+            player.play()
+        } else {
+            player.pause()
+        }
+    }
+    
+}
