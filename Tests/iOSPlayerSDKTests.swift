ac55ab53d0997833b3c171f3b194de7b6ba62ceb
Prithuvi <prithuvi@Prithuvis-MacBook-Pro.local>
2023-06-15 17:04:32 +0530

    fix: Add Test cases

--- Tests/iOSPlayerSDKTests.swift
+++ Tests/iOSPlayerSDKTests.swift
@@ -10,27 +10,25 @@ import XCTest
 
 final class iOSPlayerSDKTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testTPStreamsInitialization() throws {
+        let orgCode = "6eafqn"
+        TPStreamsSDK.initialize(withOrgCode: orgCode)
+            
+        XCTAssertEqual(TPStreamsSDK.orgCode, orgCode)
+        XCTAssertEqual(TPStreamsSDK.provider, .tpstreams)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+    func testTestpressInitialization() throws {
+        let orgCode = "lmsdemo"
+        TPStreamsSDK.initialize(for: .testpress,withOrgCode: orgCode)
+            
+        XCTAssertEqual(TPStreamsSDK.orgCode, orgCode)
+        XCTAssertEqual(TPStreamsSDK.provider, .testpress)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testInitTPAVPlayer() throws {
+        let player = TPAVPlayer(assetID: "1234567890", accessToken: "abc123")
+        XCTAssertNotNil(player)
     }
 
 }
