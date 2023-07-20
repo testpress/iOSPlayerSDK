1da412e043003b3336447890e61ab7a24190ab2c
hari-testpress <hari@testpress.in>
2023-07-20 13:46:29 +0530

    Add sample app with storyboard to our SDK

---
+++ StoryboardExample/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  StoryboardExample
+//
+//  Created by Testpress on 20/07/23.
+//
+
+import UIKit
+import TPStreamsSDK
+import AVKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var playerContainer: UIView!
+    
+    var playerViewController: AVPlayerViewController?
+    var player: AVPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupPlayer()
+    }
+    
+    func setupPlayer(){
+        player = TPAVPlayer(assetID: "8eaHZjXt6km", accessToken: "16b608ba-9979-45a0-94fb-b27c1a86b3c1")
+        playerViewController = AVPlayerViewController()
+        playerViewController?.player = player
+
+        addChild(playerViewController!)
+        playerContainer.addSubview(playerViewController!.view)
+        playerViewController!.view.frame = playerContainer.bounds
+        playerViewController!.didMove(toParent: self)
+        player?.play()
+    }
+}
+
