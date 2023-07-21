d75497636681a30f069c89b09f8f45ee00963da3
Hari <hari@testpress.in>
2023-07-21 17:11:53 +0530

    fix: Create sample app with storyboard to test our SDK (#18)
    - Created a sample app using storyboard and set minimum deployment
    version 11.4 and integrated with our player SDK.

---
+++ StoryboardExample/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  StoryboardExample
+//
+//  Created by Testpress on 20/07/23.
+//
+
+import UIKit
+import TPStreamsSDK
+import AVKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var playerContainer: UIView!
+    
+    var playerViewController: AVPlayerViewController?
+    var player: TPAVPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupPlayerView()
+        player?.play()
+    }
+    
+    func setupPlayerView(){
+        player = TPAVPlayer(assetID: "8eaHZjXt6km", accessToken: "16b608ba-9979-45a0-94fb-b27c1a86b3c1")
+        playerViewController = AVPlayerViewController()
+        playerViewController?.player = player
+
+        addChild(playerViewController!)
+        playerContainer.addSubview(playerViewController!.view)
+        playerViewController!.view.frame = playerContainer.bounds
+    }
+}
+
