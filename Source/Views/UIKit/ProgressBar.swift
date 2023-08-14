43d7be67fadb5d4cdc43eca40ee119823855fdc8
Hari <hari@testpress.in>
2023-08-14 18:10:42 +0530

    feat: Add progress bar with seek through support (#29)
    - Created a custom progress bar using UIControl and integrated it into
    the player controls view.
    - This progress bar displays both the watched and buffered progress of
    the video. Additionally, it features a draggable thumb that allows users
    to easily navigate and seek through the video content.

---
+++ Source/Views/UIKit/ProgressBar.swift
@@ -0,0 +1,112 @@
+import Foundation
+import UIKit
+
+class ProgressBar: UIControl {
+    public var player: TPStreamPlayer!{
+        didSet {
+            player.addObserver(self, forKeyPath: #keyPath(TPStreamPlayer.currentTime), options: .new, context: nil)
+        }
+    }
+    private var totalWidth: CGFloat {
+        return frame.width
+    }
+    private var isDragging = false
+    private var draggedLocation: CGFloat = 0
+    private var watchedWidth: CGFloat = 0
+    private var bufferedWidth: CGFloat = 0
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        backgroundColor = .clear
+        addTargetEvents()
+        setupTapGesture()
+    }
+    
+    private func addTargetEvents() {
+        addTarget(self, action: #selector(handleTouchDown(_:for:)), for: .touchDown)
+        addTarget(self, action: #selector(handleDrag(_:for:)), for: [.touchDragInside, .touchDragOutside, .touchDragEnter, .touchDragExit])
+        addTarget(self, action: #selector(handleTouchUpInside(_:for:)), for: .touchUpInside)
+        addTarget(self, action: #selector(handleTouchUpOutside(_:for:)), for: [.touchUpOutside, .touchCancel])
+    }
+    
+    private func setupTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        addGestureRecognizer(tapGesture)
+    }
+        
+    @objc private func handleTouchDown(_ sender: UIControl, for event: UIEvent) {
+        isDragging = true
+        updateDraggedLocation(with: event)
+        setNeedsDisplay()
+    }
+    
+    @objc private func handleDrag(_ sender: UIControl, for event: UIEvent) {
+        updateDraggedLocation(with: event)
+        setNeedsDisplay()
+    }
+    
+    @objc private func handleTouchUpInside(_ sender: UIControl, for event: UIEvent) {
+        isDragging = false
+        updateDraggedLocation(with: event)
+        setNeedsDisplay()
+        
+        let seconds = getSecondsAtPosition(draggedLocation)
+        player.goTo(seconds: seconds)
+    }
+    
+    @objc private func handleTouchUpOutside(_ sender: UIControl, for event: UIEvent) {
+        isDragging = false
+        setNeedsDisplay()
+    }
+    
+    @objc private func handleTap(_ gesture: UITapGestureRecognizer) {
+        let tapLocation = gesture.location(in: self)
+        let seconds = getSecondsAtPosition(tapLocation.x)
+        player.goTo(seconds: seconds)
+    }
+    
+    private func updateDraggedLocation(with event: UIEvent) {
+        if let touch = event.touches(for: self)?.first {
+            let touchLocation = touch.location(in: self)
+            draggedLocation = touchLocation.x
+        }
+    }
+    
+    private func getSecondsAtPosition(_ location: CGFloat) -> Float64 {
+        let percentage = Double(location / totalWidth)
+        return player.videoDuration * percentage
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == #keyPath(TPStreamPlayer.currentTime) {
+            watchedWidth = calculateWidthForValue(value: player.currentTime.doubleValue)
+            bufferedWidth = calculateWidthForValue(value: player.bufferedDuration)
+            setNeedsDisplay()
+        }
+    }
+    
+    private func calculateWidthForValue(value: Float64) -> CGFloat {
+        let percentage = CGFloat(value / player.videoDuration)
+        return totalWidth * percentage
+    }
+    
+    override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        drawBar(context, width: rect.width, color: UIColor.gray.withAlphaComponent(0.7).cgColor) // Gray background bar
+        drawBar(context, width: bufferedWidth, color: UIColor.white.withAlphaComponent(0.6).cgColor) // Buffered progress bar
+        drawBar(context, width: watchedWidth, color: UIColor.red.cgColor) // Watched progress bar
+        drawDraggableThumb(context)
+    }
+    
+    private func drawBar(_ context: CGContext, width: CGFloat, color: CGColor){
+        context.setFillColor(color)
+        context.fill(CGRect(x: 0, y: 5, width: width, height: 3))
+    }
+    
+    private func drawDraggableThumb(_ context: CGContext){
+        context.setFillColor(UIColor.red.cgColor)
+        let circleCenterX = isDragging ? draggedLocation : watchedWidth
+        let size = isDragging ? 16.0 : 14.0
+        context.fillEllipse(in: CGRect(x: circleCenterX - 9, y: frame.height / 2 - 9, width: size, height: size))
+    }
+}
