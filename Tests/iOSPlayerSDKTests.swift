e751745733cf84a02cd74b0214578906a3ebae86
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-12-27 19:31:47 +0530

    fix

--- Tests/iOSPlayerSDKTests.swift
+++ Tests/iOSPlayerSDKTests.swift
@@ -10,27 +10,18 @@ import XCTest
 
 final class iOSPlayerSDKTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testInitializeTestpress() throws {
+        TPStreamsSDK.initialize(for: .testpress, withOrgCode: "testpress")
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        XCTAssertEqual(TPStreamsSDK.orgCode, "testpress")
+        XCTAssertEqual(TPStreamsSDK.provider, .testpress)
     }
+    
+    func testInitializeTPStreams() throws {
+        TPStreamsSDK.initialize(for: .tpstreams, withOrgCode: "tpstreams")
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssertEqual(TPStreamsSDK.orgCode, "tpstreams")
+        XCTAssertEqual(TPStreamsSDK.provider, .tpstreams)
     }
 
 }
