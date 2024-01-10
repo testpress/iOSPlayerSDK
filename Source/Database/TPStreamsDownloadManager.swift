3c56c324a2811b9e2d46b0920c60940b341935bc
PruthiviRaj27 <pruthiviraj@testpress.in>
2024-01-10 15:58:30 +0530

    Added non-drm video download support

---
+++ Source/Database/TPStreamsDownloadManager.swift
@@ -0,0 +1,91 @@
+//
+//  TPStreamsDownloadManager.swift
+//  TPStreamsSDK
+//
+//  Created by Prithuvi on 08/01/24.
+//
+
+import Foundation
+import AVFoundation
+
+
+public final class TPStreamsDownloadManager {
+
+    static public let shared = TPStreamsDownloadManager()
+    private var assetDownloadURLSession: AVAssetDownloadURLSession!
+    private var assetDownloadDelegate: AssetDownloadDelegate!
+
+    private init() {
+        let backgroundConfiguration = URLSessionConfiguration.background(withIdentifier: "com.tpstreams.downloadSession")
+        assetDownloadDelegate = AssetDownloadDelegate()
+        assetDownloadURLSession = AVAssetDownloadURLSession(
+            configuration: backgroundConfiguration,
+            assetDownloadDelegate: assetDownloadDelegate,
+            delegateQueue: OperationQueue.main
+        )
+    }
+
+    internal func startDownload(asset: Asset, bitRate: Double) {
+
+        if OfflineAsset.manager.isExist(assetId: asset.id) { return }
+
+        let avUrlAsset = AVURLAsset(url: URL(string: asset.video.playbackURL)!)
+
+        guard let task = assetDownloadURLSession.makeAssetDownloadTask(
+            asset: avUrlAsset,
+            assetTitle: asset.title,
+            assetArtworkData: nil,
+            options: [AVAssetDownloadTaskMinimumRequiredMediaBitrateKey: bitRate]
+        ) else { return }
+
+        let offlineAsset = OfflineAsset.create(assetId: asset.id, srcURL: asset.video.playbackURL)
+        OfflineAsset.manager.add(object: offlineAsset)
+        assetDownloadDelegate.activeDownloadsMap[task] = offlineAsset
+        task.resume()
+    }
+
+}
+
+internal class AssetDownloadDelegate: NSObject, AVAssetDownloadDelegate {
+
+    var activeDownloadsMap = [AVAssetDownloadTask: OfflineAsset]()
+
+    public func urlSession(_ session: URLSession, assetDownloadTask: AVAssetDownloadTask, didFinishDownloadingTo location: URL) {
+        guard let offlineAsset = activeDownloadsMap[assetDownloadTask] else { return }
+        offlineAsset.update(["downloadedPath": location.relativePath])
+    }
+
+    public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        guard let assetDownloadTask = task as? AVAssetDownloadTask else { return }
+        guard let offlineAsset = activeDownloadsMap[assetDownloadTask] else { return }
+        updateDownloadCompleteStatus(error, offlineAsset)
+        activeDownloadsMap.removeValue(forKey: assetDownloadTask)
+    }
+    
+    private func updateDownloadCompleteStatus(_ error: Error?,_ offlineAsset: OfflineAsset) {
+        let status: Status = (error == nil) ? .finished : .failed
+        let updateValues: [String: Any] = ["status": status.rawValue, "downloadedAt": Date()]
+        offlineAsset.update(updateValues)
+    }
+
+    public func urlSession(_ session: URLSession,
+                           assetDownloadTask: AVAssetDownloadTask,
+                           didLoad timeRange: CMTimeRange,
+                           totalTimeRangesLoaded loadedTimeRanges: [NSValue],
+                           timeRangeExpectedToLoad: CMTimeRange
+    ) {
+        guard let offlineAsset = activeDownloadsMap[assetDownloadTask] else { return }
+
+        let percentageComplete = calculateDownloadPercentage(loadedTimeRanges, timeRangeExpectedToLoad)
+        offlineAsset.update(["status": Status.inProgress.rawValue, "percentageCompleted": percentageComplete])
+    }
+
+    private func calculateDownloadPercentage(_ loadedTimeRanges: [NSValue], _ timeRangeExpectedToLoad: CMTimeRange) -> Double {
+        var percentageComplete = 0.0
+                for value in loadedTimeRanges {
+                    let loadedTimeRange = value.timeRangeValue
+                    percentageComplete += loadedTimeRange.duration.seconds / timeRangeExpectedToLoad.duration.seconds
+                }
+        return percentageComplete * 100
+    }
+}
