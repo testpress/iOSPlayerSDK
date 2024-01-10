43d664fe43f9f39a2fad46cac9ec9ab71a1d4771
PruthiviRaj27 <112844543+PruthiviRaj27@users.noreply.github.com>
2024-01-10 15:31:20 +0530

    enhancement: Store asset in TPAVPlayer attribute(#53)
    - This commit introduces the addition of the asset field to
    `TPAVPlayer`, enabling access to the asset from any part of the player.
    - Furthermore, the asset parameters have been removed from `setup()`
    since the asset is now a class member that can be accessed directly.

--- Source/TPAVPlayer.swift
+++ Source/TPAVPlayer.swift
@@ -23,6 +23,7 @@ public class TPAVPlayer: AVPlayer {
     private var resourceLoaderDelegate: ResourceLoaderDelegate
     public var onError: ((Error) -> Void)?
     internal var initializationError: Error?
+    internal var asset: Asset? = nil
     
     public var availableVideoQualities: [VideoQuality] = [VideoQuality(resolution:"Auto", bitrate: 0)]
     
@@ -48,7 +49,8 @@ public class TPAVPlayer: AVPlayer {
             guard let self = self else { return }
             
             if let asset = asset {
-                self.setup(withAsset: asset)
+                self.asset = asset
+                self.setup()
                 self.setupCompletion?(nil)
             } else if let error = error{
                 SentrySDK.capture(error: error)
@@ -59,9 +61,9 @@ public class TPAVPlayer: AVPlayer {
         }
     }
     
-    private func setup(withAsset asset: Asset) {
-        guard let url = URL(string: asset.video.playbackURL) else {
-            debugPrint("Invalid playback URL received from API: \(asset.video.playbackURL)")
+    private func setup() {
+        guard let url = URL(string: asset!.video.playbackURL) else {
+            debugPrint("Invalid playback URL received from API: \(asset!.video.playbackURL)")
             return
         }
         
