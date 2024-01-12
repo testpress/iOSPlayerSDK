2a6f298813f9735bc836bc618e9d14bab8cbaa15
PruthiviRaj27 <pruthiviraj@testpress.in>
2024-01-12 12:31:48 +0530

    refcator

--- StoryboardExample/ViewController.swift
+++ StoryboardExample/ViewController.swift
@@ -40,7 +40,7 @@ class ViewController: UIViewController {
             .setPreferredRewindDuration(5)
             .setprogressBarThumbColor(.systemBlue)
             .setwatchedProgressTrackColor(.systemBlue)
-            .enableDownload(true)
+            .showDownloadOption()
             .build()
         
         playerViewController?.config = config
