a2bfe7247218a73d262ca8853a091837a423c353
PruthiviRaj27 <pruthiviraj@testpress.in>
2024-02-01 18:45:50 +0530

    add delete option

--- Example/AppDownloadManager.swift
+++ Example/AppDownloadManager.swift
@@ -39,6 +39,12 @@ class AppDownloadManager: TPStreamsDownloadDelegate, ObservableObject {
     func onResume(offlineAsset: OfflineAsset) {
         print("Download Resume", offlineAsset.assetId)
     }
+    
+    func onDelete(assetId: String) {
+        if let index = offlineAssets.firstIndex(where: { $0.assetId == assetId }) {
+            offlineAssets.remove(at: index)
+        }
+    }
 
     @objc func handleOfflineAssetsUpdated() {
         getOfflineAssets()
