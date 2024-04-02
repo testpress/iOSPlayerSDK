4388749d640abe84267f705349e32a8d9d419523
hari-testpress <hari@testpress.in>
2024-04-02 11:21:10 +0530

    Test

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0")),
     .package(url: "https://github.com/getsentry/sentry-cocoa.git", .upToNextMajor(from: "8.0.0")),
-    .package(url: "https://github.com/M3U8Kit/M3U8Parser", .upToNextMajor(from: "1.0.0"))
+    .package(url: "https://github.com/M3U8Kit/M3U8Parser", .upToNextMajor(from: "1.0.0")),
+    .package(url: "https://github.com/ashleymills/Reachability.swift", .upToNextMajor(from: "5.1.0"))
   ],
 
   targets: [
