c25da4839437cd7e48c74592d8963971737f70ed
hari-testpress <hari@testpress.in>
2024-06-13 13:49:03 +0530

    Refactor the code

--- Source/Network/Parsers/BaseAPIParser.swift
+++ Source/Network/Parsers/BaseAPIParser.swift
@@ -9,4 +9,6 @@ import Foundation
 
 protocol APIParser {
     func parseAsset(data: Data) throws -> Asset
+    func parseVideo(from dictionary: [String: Any]?) -> Video?
+    func parseLiveStream(from dictionary: [String: Any]?) -> LiveStream?
 }
