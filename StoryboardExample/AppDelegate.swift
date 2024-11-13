ca0e4f505a5ae41b7c5811303b04d92f4ca3e6fd
PruthiviRaj27 <pruthiviraj@testpress.in>
2024-11-13 11:07:59 +0530

    fix

--- StoryboardExample/AppDelegate.swift
+++ StoryboardExample/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        TPStreamsSDK.initialize(withOrgCode: "g2udjm")
+        TPStreamsSDK.initialize(withOrgCode: "6eafqn")
         return true
     }
 
