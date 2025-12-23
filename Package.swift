b9873a83c151a8a2c4863376738ff20ce751da43
Gowreesh-A-M <gowreesh@testpress.in>
2025-12-23 12:51:55 +0530

    update the M3U8parser version to 1.2.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1")),
-    .package(url: "https://github.com/M3U8Kit/M3U8Parser", .upToNextMajor(from: "1.1.0")),
+    .package(url: "https://github.com/M3U8Kit/M3U8Parser", .upToNextMajor(from: "1.2.0")),
     .package(url: "https://github.com/ashleymills/Reachability.swift", .upToNextMajor(from: "5.2.2")),
     .package(url: "https://github.com/realm/realm-swift", exact: "10.54.2")
   ],
